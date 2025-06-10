d3e2c483b4c08cab80eac1727079be57927f2db9
Tariq-Monowar <hassainseam@gmail.com>
2025-06-10 22:15:32 +0600

    utility function for file management

---
+++ models/users/users.routes.ts
@@ -0,0 +1,18 @@
+// import express from "express";
+// import {  } from "./users.controller";
+
+// import upload from "../../config/multer.config";
+
+
+// const router = express.Router();
+
+
+// router.post("/register", createUser);
+// router.post("/login", loginUser);
+
+
+
+
+
+
+// export default router;
